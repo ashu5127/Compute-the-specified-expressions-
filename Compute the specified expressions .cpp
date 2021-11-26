be887251a4adc5145eb3f37743f046ba71599f9b
ashu5127 <93268981+ashu5127@users.noreply.github.com>
2021-11-26 12:37:08 +0530

    Add files via upload

---
+++ Compute the specified expressions .cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float a=25.5;
+float b=3.5;
+float c=40.5;
+float d=4.5;
+//float a,b,c,d;
+//cout<<"Enter any four number"<<endl;
+//cin>>a>>b>>c>>d;
+
+cout<<"Experssion output display:"<<endl;
+cout<<(a*b-b*b)/(c-d)<<endl;
+return 0;
+
+
+}
+
